5882294001d39be45019781a167df4105ad0c01f
Nick Pafundi <nick.pafundi@gmail.com>
2013-07-20 15:09:30 -0700

    Adding migration for tasks/lists associations and db seed.  Run rake db:migrate and rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+list1 = Tasklist.create(name: "Sound")
+list2 = Tasklist.create(name: "Room")
+
+list1.tasks.create(name:"Plug in the speakers", details:"just plug them in")
+list1.tasks.create(name:"Plug in the microphones", details:"it will be fun")
+list1.tasks.create(name:"Power up the console", details:"DON'T PRESS THE LITTLE RED BUTTON")
+list2.tasks.create(name:"Set up chairs", details:"15 rows, each with 12 chairs")
+list2.tasks.create(name:"Set up tables in the back", details:"One for the sound team and one for media")
+list1.tasks.create(name:"Make the band sound amazing", details:"It's partially their job too...")
+list2.tasks.create(name:"Take a nap", details:"We know you're tired...that was a lot of chairs")
