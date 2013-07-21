ef494be17c3e446482c4a8835211fae16834fb0d
Greg Losie <glosie@gmail.com>
2013-07-21 11:59:59 -0700

    tasks controller with delete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < ApplicationController
+  respond_to :json
+
+  def destroy
+    task = Task.find params[:id]
+
+    task.destroy
+    # return empty object for Backbone
+    render json: {}
+  end
 end
